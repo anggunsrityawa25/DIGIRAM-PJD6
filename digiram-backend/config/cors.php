f5bebb00be3242b8992d239bbb9e98e5fa9e60da
anggunsrityawa25 <anggunsrityawa25@gmail.com>
2026-06-05 02:24:23 +0700

    Update cors.php

--- digiram-backend/config/cors.php
+++ digiram-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://digiram.vercel.app', 'http://127.0.0.1:5173',],
+    'allowed_origins' => ['https://digiram-pjd-6-7o4o.vercel.app', 'http://127.0.0.1:5173',],
 
     'allowed_origins_patterns' => [],
 
