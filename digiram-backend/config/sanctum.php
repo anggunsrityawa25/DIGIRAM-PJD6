518dbe145535d33a4189cb7894b6567f3af1cbe7
anggunsrityawa25 <anggunsrityawa25@gmail.com>
2026-06-05 02:33:42 +0700

    Update sanctum.php

--- digiram-backend/config/sanctum.php
+++ digiram-backend/config/sanctum.php
@@ -18,12 +18,10 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    'stateful' => explode(',', env(
+    'SANCTUM_STATEFUL_DOMAINS',
+    'localhost,localhost:3000,localhost:5173,digiram-pjd-6-7o4o.vercel.app'
+    )),
 
     /*
     |--------------------------------------------------------------------------
