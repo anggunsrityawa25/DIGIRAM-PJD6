67418a574921d54cbb408b783d2b5a4b2858967a
anggunsrityawa25 <anggunsrityawa25@gmail.com>
2026-06-05 04:02:10 +0700

    Update PengaturanAkses.tsx

--- digiram-frontend/src/components/PengaturanAkses.tsx
+++ digiram-frontend/src/components/PengaturanAkses.tsx
@@ -117,7 +117,7 @@ export function PengaturanAkses({ onLogout }: PengaturanAksesProps) {
 
     try {
       const token = localStorage.getItem('digiram_token');
-      const res = await fetch(`${API_URL}/api/user/update-profile`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/user/update-profile`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
